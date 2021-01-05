7b8bcfa1d3298e7defd634a5b7ff1d1e284dc088
lucsavasconcelos <lucasvasco8@gmail.com>
2021-01-05 17:46:35 -0300

    Finish maps-project

--- maps-project/src/Company.ts
+++ maps-project/src/Company.ts
@@ -1,12 +1,14 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class Company {
+export class Company implements Mappable {
   name: string;
   catchPhrase: string;
   location : {
     lat : number,
     lng: number
-  }
+  };
+  color: string = 'red';
 
   constructor() {
     this.name = faker.company.companyName();
@@ -15,5 +17,14 @@ export class Company {
       lat : parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
     }
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+    <h1>Company Name: ${this.name}</h1>
+    <h3>Catch Phrase: ${this.catchPhrase}</h3>
+    </div>
+    `
   }
 }
